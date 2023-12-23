dba027ea90ad815dda6f0a58d2e27578042b94d2
Dmitriy Dudko <zdimaz.dudko@gmail.com>
2023-12-23 21:07:59 +0200

    load changes

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,5 +1,7 @@
 const button = document.querySelector("#btnBurger");
+const aside = document.querySelector("#mainAside");
 
 button.addEventListener("click", () => {
   button.classList.toggle("is-active");
+  aside.classList.toggle("is-active");
 });
